db9b8d4de44ad3ff12f184a31ab7e74575e540d6
ankita gosain <mailankitagosain@gmail.com>
2022-11-08 14:39:02 -0500

    Add2noWithInput.py

--- basics/Add2noWithInput.py
+++ basics/Add2noWithInput.py
@@ -1,5 +1,4 @@
 number1=input('Enter first number:   ')
 number2=input('Enter second number:  ')
-sum(int(number1) + int(number2))
-print('sum of 2 numbers is:'sum)
-
+sum = int(number1) + int(number2)
+print('sum of 2 numbers is:', sum)
