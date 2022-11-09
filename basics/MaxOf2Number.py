10e7876b7ae6b41151758ec6dbe3c2c1472b6894
ankita gosain <mailankitagosain@gmail.com>
2022-11-09 14:29:28 -0500

    :

---
+++ basics/MaxOf2Number.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+
+def main():
+    maximum(a,b)
+
+
+def maximum(a,b):
+    if(a>b):
+        print('The maximum of 2 numbers is %s'% a)
+    else:
+        print('The maximum of 2 numbers is %s'% b)
+
+if __name__ == "__main__":
+    main()
