194cdc8ffe49c85876518282a433f79707323dba
Ankita <117608356+ankitasdet@users.noreply.github.com>
2025-03-08 13:20:28 -0500

    Update Add2noWithInput.py

--- basics/Add2noWithInput.py
+++ basics/Add2noWithInput.py
@@ -4,7 +4,7 @@ def add_two_numbers(number1, number2):
         num2 = int(number2)
         return num1 + num2
     except ValueError:
-        return "Invalid input. Please enter numeric values."
+        return "Invalid input. Please enter numeric values and change your input."
 
 # For interactive use
 def main():
